daf7148fb0d73ec0a94c819114d597f0971d03c8
BINQIU1220 <ezio12a@hotmail.com>
2024-02-23 05:59:40 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let buttonPressed = this.innerHTML;
+    buttonAnimation(buttonPressed);
+    makeSound(buttonPressed);
+  });
+}
+
+document.addEventListener("keydown", function (e) {
+  buttonAnimation(e.key);
+  makeSound(e.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(innerHTML);
+      break;
+  }
+}
+
+function buttonAnimation(key) {
+  document.querySelector(`.${key}`).classList.add("pressed");
+
+  setTimeout(function () {
+    document.querySelector(`.${key}`).classList.remove("pressed");
+  }, 100);
+}
